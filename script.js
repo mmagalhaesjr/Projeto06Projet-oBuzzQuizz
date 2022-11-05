97cc4b675703f6a4d2979e9f1e2208341e1b0ae2
Marcos Magalhães Júnior <magalhaesmarcosjr@gmail.com>
2022-11-05 18:38:26 -0300

    Versão com validação dos dados da tela 3

--- script.js
+++ script.js
@@ -3,27 +3,48 @@ let tituloNovoQuiz;
 let imgNovoQuiz;
 let qtdPerguntasNovoQuiz;
 let qtdNiveisNovoQuiz;
+let txtPergunta;
+let corPergunta;
+let respostaCorreta;
+let imagemCorreta;
+let respostaIncorreta1;
+let imagemIncorreta1;
+let respostaIncorreta2;
+let imagemIncorreta2;
+let respostaIncorreta3;
+let imagemIncorreta3;
+
+const urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const colorRegex =  /#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4})/g;
 
 function renderizarPagina1CriacaoQuizz() {
   let conteudo = document.querySelector('main');
   conteudo.innerHTML = `  <h2>Comece pelo começo</h2>
-    <form >
+    <div>
         <div class="criandoQuiz">
-            <input type="text" placeholder="Título do seu quizz"  name="" id="tituloNovoQuiz" minlength="20" maxlength="65" value="" required>
+            <input type="text" placeholder="Título do seu quizz"  name="" id="tituloNovoQuiz" minlength="6">
             <input type="url" placeholder="URL da imagem do seu quizz"name="" id="imgNovoQuiz">
-            <input type="number" placeholder="Quantidade de perguntas do quizz"name="" id="qtdPerguntasNovoQuiz" min="3">
-            <input type="text" placeholder="Quantidade de níveis do quizz"name="" id="qtdNiveisNovoQuiz" min="2">
+            <input type="number" placeholder="Quantidade de perguntas do quizz"name="" id="qtdPerguntasNovoQuiz">
+            <input type="number" placeholder="Quantidade de níveis do quizz"name="" id="qtdNiveisNovoQuiz">
         </div>
-        <button onclick="renderizarPagina2CriacaoQuizz()">Prosseguir pra criar perguntas</button>    
-    </form>`
+        <button onclick="validacaoP1()">Prosseguir pra criar perguntas</button>    
+    </div>`
     }
 
-function renderizarPagina2CriacaoQuizz() {
+function validacaoP1(){
     tituloNovoQuiz = document.querySelector('#tituloNovoQuiz').value;
     imgNovoQuiz = document.querySelector('#imgNovoQuiz').value;
     qtdPerguntasNovoQuiz = document.querySelector('#qtdPerguntasNovoQuiz').value;
     qtdNiveisNovoQuiz = document.querySelector('#qtdNiveisNovoQuiz').value;
 
+    if ((tituloNovoQuiz.length >= 20) && (tituloNovoQuiz.length <= 65) && (urlRegex.test(imgNovoQuiz) === true) && (qtdPerguntasNovoQuiz >= 3) && (qtdNiveisNovoQuiz >= 2)){
+        renderizarPagina2CriacaoQuizz();
+    }else{
+        alert("Preencha os dados corretamente")
+    }
+}
+
+function renderizarPagina2CriacaoQuizz() {
     let conteudo = document.querySelector('main');
     conteudo.innerHTML= `<h2>Crie suas perguntas</h2>`;
     
@@ -38,43 +59,64 @@ function renderizarPagina2CriacaoQuizz() {
             <div class="expandido escondido">
             <h3>Pergunta ${[i+1]}</h3>
                 <div> 
-                    <input type="text" placeholder="Texto da pergunta" name="" id="" minlength="20">
-                    <input type="text" placeholder="Cor de fundo da pergunta"name="" id="">
+                    <input type="text" placeholder="Texto da pergunta" name="" id="txtPergunta" minlength="20">
+                    <input type="text" placeholder="Cor de fundo da pergunta"name="" id="corPergunta">
                 </div>
                 <h3>Resposta correta</h3>
                 <div>
-                    <input type="text" placeholder="Resposta correta"name="" id="" minlength="1">
-                    <input type="text" placeholder="URL da imagem"name="" id="">
+                    <input type="text" placeholder="Resposta correta"name="" id="respostaCorreta" minlength="1">
+                    <input type="url" placeholder="URL da imagem"name="" id="imagemCorreta">
                 </div>
                 <h3>Respostas incorretas</h3>
                 <div class="respostasIncorretas">
                     <div>
-                        <input type="text" placeholder="Resposta incorreta 1"name="" id="">
-                        <input type="text" placeholder="URL da imagem 1"name="" id="">
+                        <input type="text" placeholder="Resposta incorreta 1"name="" id="respostaIncorreta1">
+                        <input type="url" placeholder="URL da imagem 1"name="" id="imagemIncorreta1">
                     </div>
                     <div>
-                        <input type="text" placeholder="Resposta incorreta 2"name="" id="">
-                        <input type="text" placeholder="URL da imagem 2"name="" id="">
+                        <input type="text" placeholder="Resposta incorreta 2"name="" id="respostaIncorreta2">
+                        <input type="url" placeholder="URL da imagem 2" name="" id="imagemIncorreta2">
                     </div>
                     <div>
-                        <input type="text" placeholder="Resposta incorreta 3"name="" id="">
-                        <input type="text" placeholder="URL da imagem 3"name="" id="">
+                        <input type="text" placeholder="Resposta incorreta 3"name="" id="respostaIncorreta3">
+                        <input type="url" placeholder="URL da imagem 3"name="" id="imagemIncorreta3">
                     </div>
             </div>
             </div>
         </section>`
     }
-    conteudo.innerHTML+=`<button onclick="renderizarPagina3CriacaoQuizz()" >Prosseguir pra criar níveis</button>`
+    conteudo.innerHTML+=`<button onclick="validacaoP2()" >Prosseguir pra criar níveis</button>`
 }
 
-function expandirForm(select){
-    let pai = select.parentNode;
-    pai.classList.add('escondido');
-    let divExpandida = pai.parentNode.querySelector('.expandido');
-    divExpandida.classList.remove('escondido');
+function validacaoP2(){
+
+txtPergunta = document.querySelector('#txtPergunta').value;
+corPergunta = document.querySelector('#corPergunta').value;
+respostaCorreta = document.querySelector('#respostaCorreta').value;
+imagemCorreta = document.querySelector('#imagemCorreta').value;
+respostaIncorreta1 = document.querySelector('#respostaIncorreta1').value;
+imagemIncorreta1 = document.querySelector('#imagemIncorreta1').value;
+respostaIncorreta2 = document.querySelector('#respostaIncorreta2').value;
+imagemIncorreta2 = document.querySelector('#imagemIncorreta2').value;
+respostaIncorreta3 = document.querySelector('#respostaIncorreta3').value;
+imagemIncorreta3 = document.querySelector('#imagemIncorreta3').value;
+
+  if ((txtPergunta.length > 2) && (isValidHexaCode(corPergunta)) && (respostaCorreta !== '') && (respostaIncorreta1 !== '') && (checkUrl(imagemCorreta)) && (checkUrl(imagemIncorreta1)) && respostaIncorreta2 === '' && imagemIncorreta2 === '' && respostaIncorreta3 === '' && imagemIncorreta3 === ''){
+    renderizarPagina3CriacaoQuizz();
+  }else{
+      if((txtPergunta.length > 2) && (isValidHexaCode(corPergunta)) && (respostaCorreta !== '') && (respostaIncorreta1 !== '') && (checkUrl(imagemCorreta)) && (checkUrl(imagemIncorreta1)) && (respostaIncorreta2 !== '') && (checkUrl(imagemIncorreta2)) && (respostaIncorreta3 === '' && imagemIncorreta3 === '')){
+        renderizarPagina3CriacaoQuizz();
+      }else{
+          if((txtPergunta.length > 2) && (isValidHexaCode(corPergunta)) && (respostaCorreta !== '') && (respostaIncorreta1 !== '') && (checkUrl(imagemCorreta)) && (checkUrl(imagemIncorreta1)) && (respostaIncorreta2 !== '') && (checkUrl(imagemIncorreta2)) && (respostaIncorreta3 !== '') && (checkUrl(imagemIncorreta3))){
+            renderizarPagina3CriacaoQuizz();
+          }else{
+            alert('Preencha os dados corretamente')
+          }
+      }
+    }
 }
 
-function renderizarPagina3CriacaoQuizz() {
+function renderizarPagina3CriacaoQuizz(){
     let conteudo = document.querySelector('main');
     conteudo.innerHTML= '';
     conteudo.innerHTML=`<h2>Agora, decida os níveis</h2>`
@@ -85,20 +127,31 @@ function renderizarPagina3CriacaoQuizz() {
               <h3>Nível ${[i+1]}</h3>
               <img onclick="expandirForm(this)" src="icones/vetorEditar.svg" alt="">
           </div>
-
           <div class="expandido escondido">
           <h3>Nivel ${[i+1]}</h3>
-          <input type="text" placeholder="Título do nível"  name="" id="">
-          <input type="text" placeholder="% de acerto mínima"name="" id="">
-          <input type="text" placeholder="URL da imagem do nível"name="" id="">
-          <input type="text" placeholder="Descrição do nível"name="" id="">
+          <input type="text" placeholder="Título do nível"  name="" id="tituloDoNivel">
+          <input type="text" placeholder="% de acerto mínima"name="" id="acertoMinimoNivel">
+          <input type="text" placeholder="URL da imagem do nível"name="" id="urlNivel">
+          <input type="text" placeholder="Descrição do nível"name="" id="descricaoNivel">
           </div>
       </section>
       `
   }
-    conteudo.innerHTML+= `<button onclick="renderizarPagina4CriacaoQuizz()">Prosseguir pra criar perguntas</button>`
+    conteudo.innerHTML+= `<button onclick="validacaoP3()">Prosseguir pra criar perguntas</button>`
 }
-    
+function validacaoP3(){
+
+  let tituloDoNivel = document.querySelector('#tituloDoNivel').value
+  let acertoMinimoNivel = document.querySelector('#acertoMinimoNivel').value 
+  let urlNivel = document.querySelector('#urlNivel').value
+  let descricaoNivel = document.querySelector('#descricaoNivel').value
+
+  if(tituloDoNivel.length >= 10 && acertoMinimoNivel >= 0 && acertoMinimoNivel <= 100 && acertoMinimoNivel !== '' && (checkUrl(urlNivel)) && descricaoNivel.length >= 30 ){
+    alert('ok')
+  }else{
+    alert('Preencha os dados corretamente');
+  }
+}
 
 function renderizarPagina4CriacaoQuizz() {
   let conteudo = document.querySelector('main');
@@ -112,7 +165,38 @@ function renderizarPagina4CriacaoQuizz() {
     <p onclick="window.location.reload();">Voltar pra home</p>`
 }
 
-// CÓDIGO DO DANILO PARA A PAGINA INICIAL
+//função que checa se determinada string é uma url válida
+function checkUrl(string) {
+  try {
+      let url = new URL(string)
+      return true
+  } catch(err) {
+      console.log("Invalid URL!")
+  }
+}
+
+//função que checa se determinada string é uma cor hexadecimal
+function isValidHexaCode(str) {
+if (str[0] != '#')
+    return false;
+if (!(str.length == 4 || str.length == 7))
+    return false;
+for (let i = 1; i < str.length; i++)
+    if (!((str[i].charCodeAt(0) <= '0'.charCodeAt(0) && str[i].charCodeAt(0) <= 9)
+        || (str[i].charCodeAt(0) >= 'a'.charCodeAt(0) && str[i].charCodeAt(0) <= 'f'.charCodeAt(0))
+        || (str[i].charCodeAt(0) >= 'A'.charCodeAt(0) || str[i].charCodeAt(0) <= 'F'.charCodeAt(0))))
+        return false;
+return true;
+}
+
+function expandirForm(select){
+  let pai = select.parentNode;
+  pai.classList.add('escondido');
+  let divExpandida = pai.parentNode.querySelector('.expandido');
+  divExpandida.classList.remove('escondido');
+}
+
+// ---------------------------------------------------------------- CÓDIGO DO DANILO PARA A PAGINA INICIAL
 
 const quizzesContainer = document.querySelector('.quizzes-container');
 const myQuizzes = document.querySelector('.myQuizzes');
@@ -169,3 +253,5 @@ function getQuizzes() {
 };
 
 window.addEventListener('load', getQuizzes);
+
+
